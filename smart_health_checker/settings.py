6ce12b639eebaf6ef1201feb293b63fbba86853f
Abdulrahman Abbas <abdouliabbas75@gmail.com>
2025-07-21 12:26:16 +0100

    allowed host

--- smart_health_checker/settings.py
+++ smart_health_checker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(3v8m*&x&nnonq%!upn8m9xx)&a!28-4-$b$l@#=k2e*0(liws
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
